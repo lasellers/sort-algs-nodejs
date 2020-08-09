7f7a011198abb83b04e5b8c68863708fb4654ae3
Lewis Sellers <lasellers@gmail.com>
2020-08-09 12:57:46 -0400

    v3 of selection sort

--- README.md
+++ README.md
@@ -5,6 +5,41 @@ Port of SortAlgs (which is in Java) to NodeJs w/ Typescript.
 ![sortalgs](https://github.com/lasellers/sort-algs-nodejs/blob/master/sortalgs.png)
 
 
+## Running
+
+`npm run test`
+
+
+## Sorts
+* Block
+* Bubble
+* Comb
+* Cube
+* Cycle
+* Franceschini
+* Gnome
+* Heap
+* In place Merge
+* Insertion
+* Intro
+* Library
+* Merge
+* Odd Even
+* Patience
+* Quad
+* Quick
+* Selection
+* Shell
+* Smooth
+* Tim
+* Tree
+* Unshuffle
+
+## Versions
+
+* v1.0 Ported all of the sorts at the time in the Java version of SortAlgs.
+ 
+
 ## Installation
 apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1
 libasound2 libxtst6 xauth xvfb
--- SelectionSort.spec.ts
+++ SelectionSort.spec.ts
@@ -12,7 +12,6 @@ const chai = require('chai');
 var expect = require('chai').expect;
 chai.use(require('chai-arrays'));
 
-/*
 test('swaps', () => {
     let swap = selectionSort.swaps;
     expect(swap).to.be.equal(0);
@@ -22,7 +21,7 @@ test('getSwaps', () => {
     let swap = selectionSort.getSwaps();
     expect(swap).to.be.equal(0);
 });
-*/
+
 test('sort 4 element array', () => {
     let arr = sortData.getSortDataInt4();
     let arrSorted = sortData.getSortDataInt4Sorted();
@@ -31,7 +30,6 @@ test('sort 4 element array', () => {
     expect(arr).to.be.equalTo(arrSorted);
 });
 
-/*
 test('sort 6 element array', () => {
     let arr = sortData.getSortDataInt6();
     let arrSorted = sortData.getSortDataInt6Sorted();
@@ -39,4 +37,3 @@ test('sort 6 element array', () => {
     expect(arr).to.be.array();
     expect(arr).to.be.equalTo(arrSorted);
 });
-*/
